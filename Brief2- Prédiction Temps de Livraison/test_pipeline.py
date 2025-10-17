22fdcd94397357fb45f63c8eed3639cc60d4d8b6
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-10-17 10:29:11 +0100

    modified:   "Brief2- Pr\303\251diction Temps de Livraison/EDA.ipynb" new file:   "Brief2- Pr\303\251diction Temps de Livraison/test_pipeline.py"

---
+++ Brief2- Prédiction Temps de Livraison/test_pipeline.py
@@ -0,0 +1,11 @@
+import pytest
+
+def test_shape_ligne (df,df_cleaned) :
+    
+    assert df.shape[0] == df_cleaned.shape[0] 
+
+    assert df.shape[1] == df_cleaned.shape[1]
+
+def test_mae(mae):
+    seuil = 10 
+    assert mae <= seuil
