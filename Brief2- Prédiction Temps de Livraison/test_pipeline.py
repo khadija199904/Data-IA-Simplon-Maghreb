9d3620397992d62100700bcc53822e31c01220fa
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-10-22 12:36:56 +0100

    edit file

--- Brief2- Prédiction Temps de Livraison/test_pipeline.py
+++ Brief2- Prédiction Temps de Livraison/test_pipeline.py
@@ -1,6 +1,6 @@
 import pytest
 import pandas as pd
-from pipeline import best_mae
+from pipeline import df, df_cleaned, best_mae  
 # Test unitaire :  vérifier les colonnes mal typées 
 def test_format(df):
     erreurs = []
@@ -21,9 +21,9 @@ def test_dim(df,df_cleaned) :
     print(" Test des dimensions réussi ",df_cleaned.shape)
 
 
-def test_mae(mae):
+def test_mae():
     seuil = 10 
-    assert mae <= seuil
+    assert best_mae <= seuil
     print(" Test de seuil mae réussi ")
 
 
